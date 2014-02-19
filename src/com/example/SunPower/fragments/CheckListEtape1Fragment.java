c79f7953b830be619cc9596893623b2d41cdb6d1
ChoryEduard <eduard.chory@mobilesoft365.com>
2014-02-19 16:27:46 +0200

    create fragment checklist1

--- src/com/example/SunPower/fragments/CheckListEtape1Fragment.java
+++ src/com/example/SunPower/fragments/CheckListEtape1Fragment.java
@@ -5,25 +5,52 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import com.example.SunPower.R;
 
 /**
  * Created by Eduard on 19.02.14.
  */
-public final class CheckListEtape1Fragment extends Fragment implements OnClickListener {
+public final class CheckListEtape1Fragment extends Fragment implements OnCheckedChangeListener {
+    private RadioGroup radioGroupLeft;
+    private RadioGroup radioGroupRight;
 
     @Override
     public final View onCreateView(final LayoutInflater _inflater, final ViewGroup _container, final Bundle _savedInstanceState) {
-        return _inflater.inflate(R.layout.fragment_check_list1, _container, false);
-        //fdgdsfgdfg
+        View view = _inflater.inflate(R.layout.fragment_check_list1, _container, false);
+        findView(view);
+        addCheckedChangeListener();
+        return view;
     }
 
+    public final void addCheckedChangeListener(){
+        radioGroupRight.setOnCheckedChangeListener(this);
+        radioGroupLeft.setOnCheckedChangeListener(this);
+    }
 
+    public final void findView(final View view){
+        radioGroupLeft = (RadioGroup)view.findViewById(R.id.radio_group_left);
+        radioGroupRight = (RadioGroup)view.findViewById(R.id.radio_group_right);
+    }
 
     @Override
-    public final void onClick(final View _view) {
+    public void onCheckedChanged(final RadioGroup _group, final int _checkedId) {
+        if (_group == radioGroupLeft){
+            if (radioGroupRight.getCheckedRadioButtonId() > 0){
+                radioGroupRight.clearCheck();
+            }
+        }
+        if(_group == radioGroupRight){
+            if (radioGroupLeft.getCheckedRadioButtonId() > 0){
+                radioGroupLeft.clearCheck();
+            }
 
+        }
+    }
+
+    public final int getChecked(){
+        return -1;
     }
 }
