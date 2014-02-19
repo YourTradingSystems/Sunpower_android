7b1ac03e7a16820596f7951f188d767014736582
UFOnaut <uzh.wifi@gmail.com>
2014-02-19 16:57:21 +0200

    CheckList5

--- src/com/example/SunPower/fragments/CheckListEtape1Fragment.java
+++ src/com/example/SunPower/fragments/CheckListEtape1Fragment.java
@@ -9,15 +9,11 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import com.example.SunPower.R;
 
-/**
- * Created by Eduard on 19.02.14.
- */
 public final class CheckListEtape1Fragment extends Fragment implements OnClickListener {
 
     @Override
     public final View onCreateView(final LayoutInflater _inflater, final ViewGroup _container, final Bundle _savedInstanceState) {
         return _inflater.inflate(R.layout.fragment_check_list1, _container, false);
-        //fdgdsfgdfg
     }
 
 
