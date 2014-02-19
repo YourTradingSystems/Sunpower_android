1d1a93f6e5ec05a01592dbda11aa6a71701da70d
kraftdream <kraftdream@gmail.com>
2014-02-19 17:25:26 +0200

    release login activity

---
+++ src/com/example/SunPower/HomeScreen.java
@@ -0,0 +1,23 @@
+package com.example.SunPower;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by Denis on 19.02.14.
+ */
+public final class HomeScreen extends Activity implements View.OnClickListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
