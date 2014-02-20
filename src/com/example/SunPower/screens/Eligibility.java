a98f9d5e414be72374ea1933a4a89d323a828461
kraftdream <kraftdream@gmail.com>
2014-02-20 15:32:13 +0200

    make button in home screen big

---
+++ src/com/example/SunPower/screens/Eligibility.java
@@ -0,0 +1,18 @@
+package com.example.SunPower.screens;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.SunPower.R;
+
+/**
+ * Created by Denis on 20.02.14.
+ */
+public final class Eligibility extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_eligibility);
+    }
+}
--- src/com/example/SunPower/screens/Home.java
+++ src/com/example/SunPower/screens/Home.java
@@ -1,11 +1,8 @@
 package com.example.SunPower.screens;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 
 import com.example.SunPower.R;
@@ -19,32 +16,22 @@ public final class Home extends Activity implements View.OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_screen);
-        initActionBar();
-    }
-
-    /**
-     *  set the action bar options and custom layout
-     */
-
-    public void initActionBar()
-    {
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayShowHomeEnabled(false);
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setDisplayShowCustomEnabled(true);
-        LayoutInflater inflator = (LayoutInflater) this .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View customView = inflator.inflate(R.layout.custom_action_bar, null);
-        customView.findViewById(R.id.btnHome_CAb).setVisibility(View.GONE);
-        customView.findViewById(R.id.btnWork_CAb).setOnClickListener(this);
-        actionBar.setCustomView(customView);
+        setContentView(R.layout.screen_home);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId())
         {
-            case R.id.btnWork_CAb:
+            case R.id.btnAdministrator_HS:
+                startActivity(new Intent(getBaseContext(), Administrator.class));
+                break;
+
+            case R.id.btnEligibility_HS:
+                startActivity(new Intent(getBaseContext(), Eligibility.class));
+                break;
+
+            case R.id.btnArgumentaire_HS:
                 startActivity(new Intent(getBaseContext(), Argumentarie.class));
                 break;
         }
