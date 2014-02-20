6e959a6740bde455f8aad23c7262e4712a3fe01d
kraftdream <kraftdream@gmail.com>
2014-02-20 16:08:35 +0200

    add fullscreen to activity with action bar release action bar button "Home" event

--- src/com/example/SunPower/screens/argumentarie/Argumentarie.java
+++ src/com/example/SunPower/screens/argumentarie/Argumentarie.java
@@ -11,6 +11,8 @@ import android.view.View;
 import com.example.SunPower.R;
 import com.example.SunPower.screens.Home;
 
+import java.util.zip.Inflater;
+
 /**
  * User: ZOG
  * Date: 19.02.14
@@ -35,7 +37,7 @@ public final class Argumentarie extends Activity implements View.OnClickListener
         actionBar.setDisplayShowCustomEnabled(true);
         LayoutInflater inflator = (LayoutInflater) this .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View customView = inflator.inflate(R.layout.custom_action_bar, null);
-        customView.findViewById(R.id.btnHome_CAb).setVisibility(View.GONE);
+        customView.findViewById(R.id.btnHome_CAb).setOnClickListener(this);
         customView.findViewById(R.id.btnWork_CAb).setOnClickListener(this);
         actionBar.setCustomView(customView);
     }
@@ -45,11 +47,16 @@ public final class Argumentarie extends Activity implements View.OnClickListener
         switch (v.getId())
         {
             case R.id.btnHome_CAb:
-                startActivity(new Intent(getBaseContext(), Home.class));
+                // clear the stack of activities and start the home screen
+                Intent homeIntent = new Intent(getBaseContext(), Home.class);
+                homeIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(homeIntent);
+                finish();
                 break;
 
             case R.id.btnWork_CAb:
                 startActivity(new Intent(getBaseContext(), Argumentarie.class));
+                finish();
                 break;
         }
     }
