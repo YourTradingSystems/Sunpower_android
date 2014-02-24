252733829fcd4a7d72fc1851dd664a6e6f3aac84
ZOG <vasiliy.barchiy@mobilesoft365.com>
2014-02-24 15:00:45 +0200

    added .gitignore

---
+++ src/com/makemepulse/SunPower/fragments/CheckListEtape1Fragment.java
@@ -0,0 +1,64 @@
+package com.makemepulse.sunpower.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import com.makemepulse.sunpower.R;
+
+/**
+ * Created by Eduard on 19.02.14.
+ */
+public final class CheckListEtape1Fragment extends Fragment implements OnCheckedChangeListener {
+    private RadioGroup radioGroupLeft;
+    private RadioGroup radioGroupRight;
+
+    public static CheckListEtape1Fragment create(){
+        return new CheckListEtape1Fragment();
+    }
+
+    @Override
+    public final View onCreateView(final LayoutInflater _inflater, final ViewGroup _container, final Bundle _savedInstanceState) {
+        return  _inflater.inflate(R.layout.fragment_check_list1, _container, false);
+    }
+
+    private void addCheckedChangeListener(){
+        radioGroupRight.setOnCheckedChangeListener(this);
+        radioGroupLeft.setOnCheckedChangeListener(this);
+    }
+
+    private void findViews(){
+        radioGroupLeft = (RadioGroup)getView().findViewById(R.id.rg);
+        radioGroupRight = (RadioGroup)getView().findViewById(R.id.radio_group_right);
+    }
+
+    @Override
+    public void onCheckedChanged(final RadioGroup _group, final int _checkedId) {
+        if (_group == radioGroupLeft){
+            if (radioGroupRight.getCheckedRadioButtonId() > 0){
+                radioGroupRight.clearCheck();
+            }
+        }
+        if(_group == radioGroupRight){
+            if (radioGroupLeft.getCheckedRadioButtonId() > 0){
+                radioGroupLeft.clearCheck();
+            }
+
+        }
+    }
+
+    public final int getChecked(){
+        return -1;
+    }
+
+    @Override
+    public final void onActivityCreated(final Bundle _savedInstanceState) {
+        super.onActivityCreated(_savedInstanceState);
+        findViews();
+        addCheckedChangeListener();
+    }
+}
---
+++ src/com/makemepulse/SunPower/fragments/CheckListEtape4Fragment.java
@@ -0,0 +1,53 @@
+package com.makemepulse.sunpower.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+
+import com.makemepulse.sunpower.R;
+
+public final class CheckListEtape4Fragment extends Fragment {
+
+    private String question1;
+    private String question2;
+
+    private RadioButton rbtnQuestion1Var1;
+    private RadioButton rbtnQuestion2Var1;
+    private RadioButton rbtnQuestion3Var1;
+    private RadioButton rbtnQuestion4Var1;
+    private RadioButton rbtnQuestion5Var1;
+    private RadioButton rbtnQuestion6Var1;
+
+    public static final CheckListEtape4Fragment create() {
+        return new CheckListEtape4Fragment();
+    }
+
+    @Override
+    public final View onCreateView(final LayoutInflater _inflater, final ViewGroup _container, final Bundle _savedInstanceState) {
+        View view = _inflater.inflate(R.layout.fragment_check_list4, _container, false);
+        findViews(view);
+        return view;
+    }
+
+    private final void findViews(View _view) {
+        rbtnQuestion1Var1 = (RadioButton) _view.findViewById(R.id.rbtnEtap4Question1Var1);
+        rbtnQuestion2Var1 = (RadioButton) _view.findViewById(R.id.rbtnEtap4Question2Var1);
+        rbtnQuestion3Var1 = (RadioButton) _view.findViewById(R.id.rbtnEtap4Question3Var1);
+        rbtnQuestion4Var1 = (RadioButton) _view.findViewById(R.id.rbtnEtap4Question4Var1);
+        rbtnQuestion5Var1 = (RadioButton) _view.findViewById(R.id.rbtnEtap4Question5Var1);
+        rbtnQuestion6Var1 = (RadioButton) _view.findViewById(R.id.rbtnEtap4Question6Var1);
+    }
+
+    private void saveAllFragment4Data() {
+        if (rbtnQuestion1Var1.isChecked()) question1 = "OUI";
+        else question1 = "NON";
+        if (rbtnQuestion2Var1.isChecked()) question2 = "OUI";
+        else question2 = "NON";
+
+    }
+
+}
