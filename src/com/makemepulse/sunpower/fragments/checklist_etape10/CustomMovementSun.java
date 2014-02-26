d0f596bb977567dd7c504b0c3089b0522b51ca70
ZOG <zdesbulbac9i@gmail.com>
2014-02-26 18:06:04 +0200

    deleted administrator screen, added flag "no expand ui" in login screen, renamed package checklist10

--- src/com/makemepulse/sunpower/fragments/checklist_etape10/CustomMovementSun.java
+++ src/com/makemepulse/sunpower/fragments/checklist_etape10/CustomMovementSun.java
@@ -1,4 +1,4 @@
-package com.makemepulse.sunpower.fragments.checklist10;
+package com.makemepulse.sunpower.fragments.checklist_etape10;
 
 import android.content.Context;
 import android.graphics.Canvas;
