64d008eb4766c0f22487239d4ebca9c5f1f67bc4
ChoryEduard <eduard.chory@mobilesoft365.com>
2014-02-26 11:56:28 +0200

    commit design

--- src/com/makemepulse/sunpower/fragments/CheckListEtape1Fragment.java
+++ src/com/makemepulse/sunpower/fragments/CheckListEtape1Fragment.java
@@ -3,22 +3,31 @@ package com.makemepulse.sunpower.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.RadioGroup;
-import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.RadioButton;
 import com.makemepulse.sunpower.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Eduard on 19.02.14.
  */
-public final class CheckListEtape1Fragment extends Fragment implements OnCheckedChangeListener {
-    private RadioGroup radioGroupLeft;
-    private RadioGroup radioGroupRight;
+
+public final class CheckListEtape1Fragment extends Fragment implements OnClickListener {
+
+    private List<RadioButton> radioButtonList;
 
     public static CheckListEtape1Fragment create(){
         return new CheckListEtape1Fragment();
+    }
+
+    private CheckListEtape1Fragment(){
+        radioButtonList = new ArrayList<RadioButton>();
     }
 
     @Override
@@ -26,39 +35,50 @@ public final class CheckListEtape1Fragment extends Fragment implements OnChecked
         return  _inflater.inflate(R.layout.fragment_check_list1, _container, false);
     }
 
-    private void addCheckedChangeListener(){
-        radioGroupRight.setOnCheckedChangeListener(this);
-        radioGroupLeft.setOnCheckedChangeListener(this);
-    }
 
     private void findViews(){
-        radioGroupLeft = (RadioGroup)getView().findViewById(R.id.rg);
-        radioGroupRight = (RadioGroup)getView().findViewById(R.id.radio_group_right);
+        radioButtonList.add((RadioButton)getView().findViewById(R.id.radio_btn_coastal_zone));
+        radioButtonList.add((RadioButton)getView().findViewById(R.id.radio_btn_urban));
+        radioButtonList.add((RadioButton)getView().findViewById(R.id.radio_btn_suburbs));
+        radioButtonList.add((RadioButton)getView().findViewById(R.id.radio_btn_open_space));
+        radioButtonList.add((RadioButton)getView().findViewById(R.id.radio_btn_open_terrain));
     }
 
-    @Override
-    public void onCheckedChanged(final RadioGroup _group, final int _checkedId) {
-        if (_group == radioGroupLeft){
-            if (radioGroupRight.getCheckedRadioButtonId() > 0){
-                radioGroupRight.clearCheck();
-            }
-        }
-        if(_group == radioGroupRight){
-            if (radioGroupLeft.getCheckedRadioButtonId() > 0){
-                radioGroupLeft.clearCheck();
-            }
-
-        }
+    private void setOnClickListeners(){
+        for(RadioButton rb: radioButtonList)
+            rb.setOnClickListener(this);
     }
+
 
     public final int getChecked(){
+        if (niceList(radioButtonList)){
+            for(int i = 0; i < radioButtonList.size(); i++){
+                if(radioButtonList.get(i).isChecked())
+                    return i;
+            }
+        }
         return -1;
+    }
+
+    private boolean niceList(final List<RadioButton> list){
+        return list != null && !list.isEmpty();
     }
 
     @Override
     public final void onActivityCreated(final Bundle _savedInstanceState) {
         super.onActivityCreated(_savedInstanceState);
         findViews();
-        addCheckedChangeListener();
+        setOnClickListeners();
+    }
+
+    @Override
+    public void onClick(final View _view) {
+        for(RadioButton rb: radioButtonList){
+            if(_view == rb)
+                rb.setChecked(true);
+            else
+                rb.setChecked(false);
+        }
+        Log.i("Click", "> " + getChecked());
     }
 }
--- src/com/makemepulse/sunpower/global/Constants.java
+++ src/com/makemepulse/sunpower/global/Constants.java
@@ -25,4 +25,14 @@ public abstract class Constants {
     public static final int COMPASS_PARTY_No = 5;
     public static final int COMPASS_PARTY_O = 6;
     public static final int COMPASS_PARTY_So = 7;
+
+    /**
+     * returns checked radio button
+     * for CheckList1
+     */
+    public static final int CHECKED_RB_COASTAL_ZONE = 0;
+    public static final int CHECKED_RB_URBAN = 1;
+    public static final int CHECKED_RB_SUBURBS = 2;
+    public static final int CHECKED_RB_OPEN_SPACE = 3;
+    public static final int CHECKED_RB_OPEN_TERRAIN = 4;
 }
